a8c7184ce828f4ab788fe2f0679719cc13740b66
radin-reth <radin.reth@gmail.com>
2017-06-19 15:49:35 +0700

    add enum

---
+++ enum.ts
@@ -0,0 +1,11 @@
+// starts from 0
+enum Color { Red, Green, Blue };
+let myHorse: Color = Color.Red;
+
+enum Color1 { Red=1, Green, Blue };
+let myHorse1: Color1 = Color1.Green;
+console.log( myHorse1 );
+
+enum Color2 { Red=1, Green=3, Blue=5 };
+let myHorse2: Color2 = Color2.Blue;
+console.log( myHorse2 );
